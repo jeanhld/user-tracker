7db558cf3c2eaa37c16ea4157235d7744eaa0dc2
Jean Holderbaum <jeanhrs@hotmail.com>
2015-11-28 13:52:33 -0200

    Added database seeds and updated README.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128144742) do
+ActiveRecord::Schema.define(version: 20151128154450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20151128144742) do
     t.integer  "visitor_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.date     "accessed_at"
+    t.datetime "accessed_at"
   end
 
   create_table "visitors", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+(1..12).each do |i|
+  visitor=Visitor.create(identifier: "#{i}-uid")
+  Contact.create(visitor: visitor, email: "mail#{i}@mail.com")
+  (0..3).each do |j|
+    visited_page = VisitedPage.create(visitor: visitor, page: j, accessed_at: DateTime.now-j.days)
+  end
+end
